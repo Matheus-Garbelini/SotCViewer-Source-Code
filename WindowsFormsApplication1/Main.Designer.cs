1691692d0268c75f541b9634b789193ec4ee1286
Matheus Eduardo <mgarbelix@hotmail.com>
2014-08-10 21:01:33 -0300

    SotC Viewer Source Code

--- WindowsFormsApplication1/Main.Designer.cs
+++ WindowsFormsApplication1/Main.Designer.cs
@@ -271,6 +271,7 @@
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.Location = new System.Drawing.Point(372, 201);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(126, 23);
